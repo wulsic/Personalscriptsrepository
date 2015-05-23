6b284b918dd64db71fcc12f82721d4be8ce5ec2e
Wulsic <wulsic@hotmail.com>
2015-05-23 18:21:53 +0200

    RAINBOW TIME! Did I say Rainbow? Oh I meant a shitty loop of setting background color and echo-ing it.

---
+++ rainbow.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in {1..100}
+do
+tput setab 1 && sleep 0.2 && echo && tput setab 1 && sleep 0.2 && echo && tput setab 2 && sleep 0.2 && echo && tput setab 2 && sleep 0.2 && echo && tput setab 3 && sleep 0.2 && echo && tput setab 3 && sleep 0.2 && echo && tput setab 4 && sleep 0.2 && echo && tput setab 4 && sleep 0.2 && echo && tput setab 5 && sleep 0.2 && echo && tput setab 5 && sleep 0.2 && echo && tput setab 6 && sleep 0.2 && echo && tput setab 6 && sleep 0.2 && echo && tput setab 7 && sleep 0.2 && echo && tput setab 7 && sleep 0.2 && echo && tput setab 9 && sleep 0.2 && echo && tput setab 9 && sleep 0.2 && echo && tput setab 9 && sleep 0.2 && echo && tput setab 0 && sleep 0.2 && echo
+done
